8165273d0866250e06d64666fafc19487b4920cb
subh1416 <subhadipsamanta1416@gmail.com>
2022-11-17 12:30:22 +0530

    Search done

--- new/js/cart.js
+++ new/js/cart.js
@@ -18,32 +18,22 @@ function ready() {
   }
 
   var addToCartButtons = document.getElementsByClassName("shop-item-button");
-  
 
-  
   for (var i = 0; i < addToCartButtons.length; i++) {
     var button = addToCartButtons[i];
     button.addEventListener("click", addToCartClicked);
-    
   }
- 
 
   // document
   //   .getElementsByClassName("btn-purchase")[0]
   //   .addEventListener("click", purchaseClicked);
 }
 
-
-
 // function purchaseClicked() {
 //   alert("Thankyou for your purchase");
 //   localStorage.clear();
-  
+
 // }
-
-
-
-
 
 // function quantityChanged(event) {
 //   var input = event.target;
@@ -59,76 +49,58 @@ function addToCartClicked(event) {
   var shopItem = button.parentElement.parentElement;
   var title = shopItem.getElementsByClassName("shop-item-title")[0].innerText;
   var price = shopItem.getElementsByClassName("shop-item-price")[0].innerText;
- 
 
-  
   let cart = localStorage.getItem("cart");
-  if(cart==null){
-    let products=[];
-    let product={title,price,productQuantity:1};
+  if (cart == null) {
+    let products = [];
+    let product = { title, price, productQuantity: 1 };
     products.push(product);
-    localStorage.setItem("cart",JSON.stringify(products));
+    localStorage.setItem("cart", JSON.stringify(products));
     console.log("item added for the first time");
-
-  }
-
- 
-  else{
+  } else {
     let pcart = JSON.parse(cart);
     let oldProduct = pcart.find((item) => item.title == title);
 
-    if(oldProduct){
-        oldProduct.productQuantity = oldProduct.productQuantity + 1
-        pcart.map((item) => {
-          if (item.title == oldProduct.title){
-            item.productQuantity = oldProduct.productQuantity;
-          }
-        })
-        localStorage.setItem("cart",JSON.stringify(pcart));
-        console.log("quantity increased");
-      }
-
-    else{
-      let product={title,price,productQuantity:1};
+    if (oldProduct) {
+      oldProduct.productQuantity = oldProduct.productQuantity + 1;
+      pcart.map((item) => {
+        if (item.title == oldProduct.title) {
+          item.productQuantity = oldProduct.productQuantity;
+        }
+      });
+      localStorage.setItem("cart", JSON.stringify(pcart));
+      console.log("quantity increased");
+    } else {
+      let product = { title, price, productQuantity: 1 };
       pcart.push(product);
-      localStorage.setItem("cart",JSON.stringify(pcart));
+      localStorage.setItem("cart", JSON.stringify(pcart));
       console.log("item added");
-
-
     }
-    
-   
   }
 
   console.log(title, price);
-  
+
   // addToCart(title, price,productQuantity);
   // addToCart()
 }
 
-
 //remove button
 // function removeCartItem() {
-  
-  
+
 //   let cart=JSON.parse(localStorage.getItem('cart'));
 //   let newcart= cart.filter((item)=>item.title != title);
 //   localStorage.setItem('cart',JSON.stringify(newcart));
 //   addToCart();
- 
+
 // }
 
-function addToCart()
-{
+function addToCart() {
   let cartString = localStorage.getItem("cart");
   let cart = JSON.parse(cartString);
-  if(cart==null || cart.length==0){
+  if (cart == null || cart.length == 0) {
     console.log("cart is empty");
     // $(".text1").html("<h3> hua </h3>");
-    
-    
-  }
-  else{
+  } else {
     console.log(cart);
     let table = `
     <table class='table'>
@@ -145,22 +117,24 @@ function addToCart()
     `;
 
     let totalPrice = 0;
-    cart.map((item)=>{
-      table+=`
+    cart.map((item) => {
+      table += `
       <tr>
         <td> ${item.title}</td>
         <td> ${item.productQuantity}</td>
-        <td> ${item.productQuantity*item.price}</td>
+        <td> ${item.productQuantity * item.price}</td>
        
       
       </tr>
       
       
-      `
-      totalPrice+=item.price*item.productQuantity
-    })
+      `;
+      totalPrice += item.price * item.productQuantity;
+    });
 
-    table = table + ` </table>
+    table =
+      table +
+      ` </table>
     <div class="cart-total">
         <strong class="cart-total-title">Total</strong>
         <span class="shoping">&#8377</span>
@@ -168,19 +142,10 @@ function addToCart()
     </div>
     
     
-   `
+   `;
     $(".cart-row").html(table);
-
-   
-    
-
-
   }
-
-  
-
 }
-
 
 // function addToCart(title, price,productQuantity) {
 //   var cartRow = document.createElement("div");
@@ -188,7 +153,6 @@ function addToCart()
 //   var cartItems = document.getElementsByClassName("cart-items")[0];
 //   var cartItemName = cartItems.getElementsByClassName("cart-item-title");
 //   let cartString = localStorage.getItem("cart")
-  
 
 //   for (var i = 0; i < cartItemName.length; i++) {
 //     if (cartItemName[i].innerText == title) {
@@ -234,8 +198,46 @@ function addToCart()
 //   document.getElementsByClassName("cart-total-price")[0].innerText = total;
 // }
 
-function addClick(){
-    alert("Item added to cart")
+function addClick() {
+  alert("Item added to cart");
 }
 
-addToCart();
+function search() {
+  let input = document.getElementById("search1").value;
+  input = input.toLowerCase();
+  let x = document.getElementsByClassName("box");
+  let y = document.getElementsByClassName("slideshow-container");
+  let z = document.getElementsByClassName("heading");
+  let a = document.getElementsByClassName("dot");
+  
+
+  if (input!="") {
+    for (i = 0; i < x.length; i++) {
+      if (!x[i].innerHTML.toLowerCase().includes(input)) {
+        x[i].style.display = "none";
+
+      } else {
+        x[i].style.display = "inline-block";
+
+        for (let i = 0; i < y.length; i++) {
+          y[i].style.display = "none";
+        }
+        for (let i = 0; i < z.length; i++) {
+          z[i].style.display = "none";
+        }
+        for (let i = 0; i < a.length; i++) {
+          a[i].style.display = "none";
+        }
+      }
+    }
+  } else {
+    for (let i = 0; i < y.length; i++) {
+      y[i].style.display = "block";
+    }
+    for (let i = 0; i < z.length; i++) {
+      z[i].style.display = "block";
+    }
+  }
+
+  addToCart();
+}
